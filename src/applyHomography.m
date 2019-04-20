3f483b5f9f4e29a40f4c2a569092e4cb3db35a32
LichengxiHuang <lichengxi.huang@gmail.com>
2019-04-19 23:19:41 -0500

    remove matlab files

--- src/applyHomography.m
+++
@@ -1,9 +0,0 @@
-function dest_pts_nx2 = applyHomography(H_3x3, src_pts_nx2)
-num_pairs = size(src_pts_nx2, 1);
-
-dest = H_3x3 * vertcat(src_pts_nx2', ones(1,num_pairs));
-
-dest_pts_nx2 = [
-    dest(1,:) ./ dest(3,:);
-    dest(2,:) ./ dest(3,:)
-    ]';
